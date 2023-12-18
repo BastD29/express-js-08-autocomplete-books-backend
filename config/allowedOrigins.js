d7dfd1e02da224f07040bdc598d16fb8de8a0a46
BastD29 <bastiendaniel9@gmail.com>
2023-12-18 15:14:15 +0100

    added cors origin

---
+++ config/allowedOrigins.js
@@ -0,0 +1,4 @@
+// const allowedOrigins = ["https://technotes.onrender.com"];
+const allowedOrigins = ["https://autocomplete-books-api.onrender.com"];
+
+module.exports = allowedOrigins;
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
 connectDB();
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
